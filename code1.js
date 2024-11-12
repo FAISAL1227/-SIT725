b008e10cbf758e7e935eff761a2a712a4f8bec3c
Faisal Ibrahim Syed <120768886+FAISAL1227@users.noreply.github.com>
2024-11-12 22:46:27 +1100

    code1

---
+++ code1.js
@@ -0,0 +1,15 @@
+// Sum numbers from 1 to 10
+let sum = 0;
+for (let i = 1; i <= 10; i++) {
+    sum += i;
+}
+
+// Display the result
+console.log("The sum of numbers from 1 to 20 is=", sum);
+
+// Check if the sum is even or odd
+if (sum % 2 === 0) {
+    console.log("The sum is even.");
+} else {
+    console.log("The sum of numbers is odd.");
+}
